7a9661d0b4ece86be633a7ef9219425a4fd491c1
Farizgeloz <farizgeloz26@gmail.com>
2026-01-01 00:53:02 +0700

    1225 commit

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api_url_satudata = axios.create({
   /* baseURL: "https://api.mataprabulinggih.net/api/v1/public/", */
-  baseURL: "/api-mataprabu/",
+  baseURL: "https://apisatudata.probolinggokab.go.id/api/v1/public/",
   headers: {
     "Content-Type": "application/json",
   },
@@ -11,7 +11,7 @@ const api_url_satudata = axios.create({
 const api_url_satuadmin = axios.create({
  /*  baseURL: "http://localhost:3000/api/", */
   /* baseURL: "https://api-satu.mataprabulinggih.net/", */
-  baseURL: "/api/",
+  baseURL: "https://apisatuadmin.probolinggokab.go.id/api/",
   headers: {
     "Content-Type": "application/json",
   },
