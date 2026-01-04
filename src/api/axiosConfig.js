5ea6cd6f854f941b8c711034dc44f079b66064e5
Farizgeloz <farizgeloz26@gmail.com>
2026-01-04 14:18:12 +0700

    0501 commit new

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -9,15 +9,13 @@ const api_url_satudata = axios.create({
 // API SatuAdmin (read)
 const api_url_satuadmin = axios.create({
   baseURL: "https://apisatuadmin.probolinggokab.go.id/api/",
-  headers: { "Content-Type": "application/json" },
-  withCredentials: true,
+  headers: { "Content-Type": "application/json" }
 });
 
 // API SatuAdmin (create/post)
 const api_url_satuadmin_create = axios.create({
   baseURL: "https://apisatuadmin.probolinggokab.go.id/api/",
-  headers: { "Content-Type": "application/json" },
-  withCredentials: true,
+  headers: { "Content-Type": "application/json" }
 });
 
 export { api_url_satudata, api_url_satuadmin, api_url_satuadmin_create };
